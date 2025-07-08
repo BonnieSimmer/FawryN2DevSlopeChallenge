dfb4f04579ab742e784c52730ad1bd878fa4164f
bonniesimmer <noura19918@gmail.com>
2025-07-08 17:59:19 +0300

    Ebook class constructor and methods and ShowcaseDemoBook class constructor

--- src/allBooks/Ebook.java
+++ src/allBooks/Ebook.java
@@ -1,4 +1,20 @@
 package allBooks;
 
-public class Ebook extends Book {
+import java.time.Year;
+
+public class Ebook extends Book implements Emailable {
+    String fileType;
+
+    public Ebook(String isbn, String title, Year year, double price, String fileType) {
+        super(isbn, title, year, price);
+        this.fileType = fileType;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+    @Override
+    public void sendToEmail() {
+        System.out.println(title + " has been sent to your email.");
+    }
 }
--- src/allBooks/ShowcaseDemoBook.java
+++ src/allBooks/ShowcaseDemoBook.java
@@ -1,4 +1,9 @@
 package allBooks;
 
+import java.time.Year;
+
 public class ShowcaseDemoBook extends Book {
+    public ShowcaseDemoBook(String isbn, String title, Year publishYear, double price) {
+        super(isbn, title, publishYear, price);
+    }
 }
